36a5eb413cb8dcdc0611153feb63b300ac7acfa7
Yvon Rakotonanahary <yvon.rakotonanahary@gmail.com>
2024-09-01 22:32:09 +0200

    All sections

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'about', component: AboutComponent },
+    { path: 'experience', component: ExperienceComponent },
+    { path: 'education', component: EducationComponent },
+    { path: 'skills', component: SkillsComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '', redirectTo: '/about', pathMatch: 'full' },
+];
